2123ab7efbd7bd5c17ef66bd85bd6b655932222f
kawolski <prashantxrawat@gmail.com>
2021-03-22 17:26:21 +0530

    --> Test Version

--- lib/screens/add_devices/add_devices.dart
+++ lib/screens/add_devices/add_devices.dart
@@ -46,107 +46,79 @@ class _SettingsState extends State<AddDevices> {
                           )
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 50),
-                              color: Colors.yellowAccent.withOpacity(0.5),
-                              child: TextButton.icon(
-                                icon: Icon(Icons.lightbulb_outline_sharp, size: 50),
-                                label: Text("Light"),
-                                onPressed: () {
-                                  showDialog(
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          contentPadding: EdgeInsets.symmetric(
-                                              vertical: 10, horizontal: 10),
-                                          content: Column(
-                                            mainAxisAlignment:
-                                            MainAxisAlignment.spaceEvenly,
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: <Widget>[
-                                              Text('Light'),
-                                              SizedBox(height: 50),
-                                              SwitchForm(
-                                                realDB: realDB,
-                                                type: 'Light',
-                                              )
-                                            ],
-                                          ),
-                                        );
-                                      });
-                                },
-                              )),
+                    Card(
+                      margin: EdgeInsets.fromLTRB(5, 6, 5, 0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.lightbulb_outline_sharp,
+                          color: Colors.black87,
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 50),
-                              color: Colors.greenAccent.withOpacity(0.4),
-                              child: TextButton.icon(
-                                  icon: Icon(Icons.swipe, size: 50),
-                                  label: Text("Switches"),
-                                  onPressed: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return AlertDialog(
-                                            contentPadding: EdgeInsets.symmetric(
-                                                vertical: 10, horizontal: 10),
-                                            content: Column(
-                                              mainAxisAlignment:
-                                              MainAxisAlignment.spaceEvenly,
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: <Widget>[
-                                                Text('Switches'),
-                                                SizedBox(height: 50),
-                                                SwitchForm(
-                                                  realDB: realDB,
-                                                  type: 'Switch',
-                                                )
-                                              ],
-                                            ),
-                                          );
-                                        });
-                                  })),
-                        ),
-                      ],
+                        title: Text('Light',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            )),
+                        onTap: (){
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 10),
+                                  content: Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text('Light'),
+                                      SizedBox(height: 50),
+                                      SwitchForm(
+                                        realDB: realDB,
+                                        type: 'Light',
+                                      )
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 50),
-                              color: Colors.redAccent.withOpacity(0.4),
-                              child: TextButton.icon(
-                                icon: Icon(Icons.account_tree_sharp, size: 50),
-                                label: Text("Sensors"),
-                                onPressed: () {
-                                  AlertDialog(title: Text('Under Construction'));
-                                },
-                              )),
+                    Card(
+                      margin: EdgeInsets.fromLTRB(5, 6, 5, 0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.swipe,
+                          color: Colors.black87,
                         ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 50),
-                              color: Colors.deepPurpleAccent.withOpacity(0.4),
-                              child: TextButton.icon(
-                                icon: Icon(Icons.photo_camera_front, size: 50),
-                                label: Text("Camera"),
-                                onPressed: () {
-                                  AlertDialog(title: Text('Under Construction'));
-                                },
-                              )),
-                        ),
-                      ],
+                        title: Text('Switch',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            )),
+                        onTap: (){
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 10, horizontal: 10),
+                                  content: Column(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      Text('Switches'),
+                                      SizedBox(height: 50),
+                                      SwitchForm(
+                                        realDB: realDB,
+                                        type: 'Switch',
+                                      )
+                                    ],
+                                  ),
+                                );
+                              });
+                        },
+                      ),
                     ),
+
                   ],
                 ),
               ),
